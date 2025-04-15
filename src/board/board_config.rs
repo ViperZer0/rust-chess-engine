f7cfeb97981c1c228a2280c95dad9b284c718db7
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-04-14 17:46:05 -0700

    Added CastlingAvailability struct

--- src/board/board_config.rs
+++ src/board/board_config.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, fmt::Display};
 
-use super::{Piece, Square};
+use super::{Piece, PlayerColor, Square};
 
 /// A specified arrangement of pieces.
 ///
@@ -18,6 +18,33 @@ pub struct BoardConfiguration
 {
     // I'm not sure if a vec or a hashmap is better here.
     pieces: HashMap<Square, Piece>,
+    active_color: PlayerColor,
+    castling_availability: CastlingAvailability,
+    en_passant_target_square: Option<Square>,
+    halfmove_clock: u8,
+    fullmove_number: u8,
+}
+
+/// This struct records information on who can castle and where.
+/// 
+/// In chess, castling is only available to a player if:
+/// - The king has not moved from its starting square
+/// - The rook on the side being castled towards has not moved.
+/// 
+/// FEN notation does not include information on the previous state of the board,
+/// and so has a dedicated field to indicate whether or not the players
+/// are allowed to castle or not.
+///
+/// Note that this doesn't include information on *temporary* scenarios in which castling are
+/// prevented. If castling would put the king in check, the option is still available to the king
+/// later.
+#[derive(Debug, PartialEq)]
+pub struct CastlingAvailability
+{
+    white_castle_kingside: bool,
+    white_castle_queenside: bool,
+    black_castle_kingside: bool,
+    black_castle_queenside: bool,
 }
 
 impl BoardConfiguration
