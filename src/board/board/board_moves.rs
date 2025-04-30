9fc9cb1106a22bc876d41ec05f4c6a3f8084aad0
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-04-30 11:57:00 -0700

    Added knight move bitmask implementation

--- src/board/board/board_moves.rs
+++ src/board/board/board_moves.rs
@@ -17,9 +17,56 @@ use crate::{bitboard::Bitboard, board::Square};
 use super::Board;
 
 impl Board {
+    /// Generates a bitboard where all valid squares that a knight on a given square can move to are set to 1.
+    /// The square that the knight is currently on is not included in this set.
+    ///
+    /// # Arguments
+    ///
+    /// * `from` - The square that the knight is on.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let knight_moves: Bitboard = Board::knight_moves(Square::new(0,0));
+    /// let knight_move_squares: Vec<Square> = knight_moves.squares().collect();
+    /// // This should be 2 since a knight in the corner only ever has two moves.
+    /// assert_eq!(knight_move_squares.len(), 2);
+    /// assert_eq!(knight_move_squares, vec![Square::new(1, 2), Square::new(2, 1)]);
+    /// ```
     fn knight_moves(from: Square) -> Bitboard
     {
-        todo!();
+        // For each possible knight move, we want to mask out the areas of the board that the
+        // knight can't move from.
+        // I.e if the knight is on the edge of the board, we can't move further to the left.
+        let knight_on_rank_1_or_higher = Bitboard::from(from) & Bitboard::rank_mask_iter(1..8);
+        let knight_on_rank_2_or_higher = Bitboard::from(from) & Bitboard::rank_mask_iter(2..8);
+        let knight_on_rank_6_or_lower  = Bitboard::from(from) & Bitboard::rank_mask_iter(0..7);
+        let knight_on_rank_5_or_lower  = Bitboard::from(from) & Bitboard::rank_mask_iter(0..6);
+        let knight_on_file_b_to_h      = Bitboard::from(from) & Bitboard::file_mask_iter(1..8);
+        let knight_on_file_c_to_h      = Bitboard::from(from) & Bitboard::file_mask_iter(2..8);
+        let knight_on_file_a_to_g      = Bitboard::from(from) & Bitboard::file_mask_iter(0..7);
+        let knight_on_file_a_to_f      = Bitboard::from(from) & Bitboard::file_mask_iter(0..6);
+
+        // Each bitshift is the number of squares between the source and target square
+        // With a bitmask to block out "bad" areas that can't be moved from.
+        let north_north_east = (knight_on_rank_5_or_lower & knight_on_file_a_to_g) << 17;
+        let north_east_east = (knight_on_rank_6_or_lower & knight_on_file_a_to_f) << 10;
+        let south_east_east = (knight_on_rank_1_or_higher & knight_on_file_a_to_f) >> 6;
+        let south_south_east = (knight_on_rank_2_or_higher & knight_on_file_a_to_g) >> 15;
+        let south_south_west = (knight_on_rank_2_or_higher & knight_on_file_b_to_h) >> 17;
+        let south_west_west = (knight_on_rank_1_or_higher & knight_on_file_c_to_h) >> 10;
+        let north_west_west = (knight_on_rank_6_or_lower & knight_on_file_c_to_h) << 6;
+        let north_north_west = (knight_on_rank_5_or_lower & knight_on_file_b_to_h) << 15;
+
+        Bitboard::default() 
+            | north_north_east
+            | north_east_east
+            | south_east_east
+            | south_south_east
+            | south_south_west
+            | south_west_west
+            | north_west_west
+            | north_north_west
     }
 }
 
